ab495a33374c8c91f495186263fde2e4ae195ead
david dobson <david.m.dobson@hotmail.com>
2019-10-10 16:18:39 -0400

    gatsby tutorial

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,4 +6,32 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "Gatsby-Bootstrap",
+    description: 'this is description',
+    keywords: 'gatsby, bes, nes',
+    image: '/static/nes.jpg',
+    url:'https://www.gatsbyjs.org/'
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        baseUrl: `codingsrc.com`,
+        protocol: `http`,
+        hostingWPCOM: false,
+      },
+    },
+    `gatsby-plugin-react-helmet`
+  ],
 }
---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PrimaryLayout from "../layouts/PrimaryLayout"
+
+
+export default () => (
+ <PrimaryLayout column='col-xs-6'>
+ <h1>About Us</h1>
+         <p>djkfjadklfdafldajflkajf;adjfkldjasf;lajfjlas;fjsakljfkldsajfkasdjfkladsjfklasdjf;lasd</p>
+ </PrimaryLayout>
+  
+);
